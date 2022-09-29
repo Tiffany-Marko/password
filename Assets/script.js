ab2179f78ef67439d0d49a69ffc032ea9af20ad9
Tiffany Marko <tiffanyjmarko@gmail.com>
2022-09-28 20:02:59 -0700

    adding comments and pw gen

--- Assets/script.js
+++ Assets/script.js
@@ -46,20 +46,55 @@ function writePassword() {
   passwordLength = window.prompt("Please select a password length between 8 and 128 characters.");
   var wantsUppercase = window.confirm("Do you want uppercase letters in your password?");
   var wantsLowercase = window.confirm("Do you want lowercase letters in your password?");
-  // var wantsSpecialCharacter = window.confirm("Do you want special characters in your password?");
-  // var wantsNumbers = window.confirm("Do you want numbers in your password?");
+  var wantsSymbols = window.confirm("Do you want speacial characters in your password?");
+  var wantsNumbers = window.confirm("Do you want numbers in your password?");
+  // var wantsNumbers (etc) = window.confirm("Do you want numbers in your password?");
 
-  if(wantsUppercase) {
-    // add uppercase characters to masterArray
-    masterArray = masterArray.concat(uppercaseChar);
-    // console.log('masterArray now holds', masterArray);
+//user confirms all character types
+  if(wantsUppercase && wantsLowercase && wantsSymbols && wantsNumbers ) {
+   
+    masterArray = masterArray.concat(uppercaseChar, lowercaseChar, specialChars, numChars);
+     console.log('masterArray now holds', masterArray);
+     generatePassword(passwordLength)
+    
   }
+  //user excludes uppercase
+  if(!wantsUppercase && wantsLowercase && wantsSymbols && wantsNumbers) {
+    masterArray.concat(lowercaseChar, specialChars, numChars);
+  } 
+  //user excludes lowercase AND lowercase
+  if(!wantsUppercase && !wantsLowercase && wantsSymbols && wantsNumbers) {
+    masterArray.concat(specialChars, numChars);
+  }
+  //user excludes special chars
+  if(wantsUppercase && wantsLowercase && !wantsSymbols && wantsNumbers) {
+    masterArray.concat(lowercaseChar, numChars, uppercaseChar);
+  }
+//exclude numbers
+if(wantsUppercase && wantsLowercase && wantsSymbols && !wantsNumbers) {
+  masterArray.concat(lowercaseChar, specialChars, uppercaseChar);
+}
+//excludes number and special chars
+if(wantsUppercase && wantsLowercase && !wantsSymbols && !wantsNumbers) {
+  masterArray.concat(lowercaseChar, uppercaseChar);
+}
+//confirm only numbers
+if(!wantsUppercase && !wantsLowercase && !wantsSymbols && wantsNumbers) {
+  masterArray.concat(wantsNumbers);
+}
+//confirms only lowercase
 
+if(!wantsUppercase && wantsLowercase && wantsSymbols && wantsNumbers) {
+  masterArray.concat(lowercaseChar, specialChars, numChars);
+}
   if(wantsLowercase) {
     // add lowercase characters to masterArray
     masterArray = masterArray.concat(lowercaseChar);
     // console.log('masterArray now holds', masterArray);
+    console.log(masterArray); 
   }
+  
+
 
   // follow the above pattern for numbers and special characters.
   // after this, you will have an array will all the desired characters
@@ -74,19 +109,23 @@ function writePassword() {
 
 }
 // we need another function that creates a password - hint: generatePassword() on line 7
-function generatePassword() {
+function generatePassword(length) {
   // any code I want to execute WHEN this generatePassword() function is invoked/executed
   // inside this function, you can randomly select characters from your masterArray,
   // and repeat this selection as many times as the length of the variable passwordLength
   // use a for loop for repeating this random selection of characters
-  for (var i = 0; i < passwordLength; i++) {
+  var result = ""
+  for (var i = 0; i <length; i++) {
+    result += masterArray.charAt(Math.floor(Math.random() * masterArray.length));
     // the code you write inside these brackets will
     // be executed mutliples times; i.e. as many times 
     // as the condition i < passwordLength is true
     // you need to figure out how to randomly select a character/item from an array
     // and then add that character to the variable generatedPassword.
     
-  }
+  } 
+  document.querySelector("#password").innerHTML=result
+  return 
 
   // return the generated password
 }
